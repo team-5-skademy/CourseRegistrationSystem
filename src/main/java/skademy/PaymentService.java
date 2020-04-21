0505042f46625d492d34a79b508bb5fa05917ee0
bluecap2 <zunestar@gmail.com>
2020-04-21 14:43:16 +0900

    결재 req/res

--- src/main/java/skademy/PaymentService.java
+++ src/main/java/skademy/PaymentService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Service
-@FeignClient(name ="paymentSystems", url="localhost:8083/paymentSystems")
+@FeignClient(name ="paymentSystems", url="localhost:8083")
 public interface PaymentService {
     @RequestMapping(method = RequestMethod.POST, value = "/paymentSystems", consumes = "application/json")
     void makePayment(PaymentSystem paymentSystem);
