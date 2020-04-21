07c10418648feadff142909602e78b8664310acc
bluecap2 <zunestar@gmail.com>
2020-04-21 17:55:58 +0900

    EnableFeignClients

--- src/main/java/skademy/PaymentService.java
+++ src/main/java/skademy/PaymentService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Service
-@FeignClient(name ="paymentSystems", url="localhost:8083")
+@FeignClient(name ="paymentSystems", url="http://52.231.118.204:8080")
 public interface PaymentService {
     @RequestMapping(method = RequestMethod.POST, value = "/paymentSystems", consumes = "application/json")
     void makePayment(PaymentSystem paymentSystem);
